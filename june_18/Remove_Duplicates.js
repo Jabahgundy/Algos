c4d655663c79ef0de1c36b873fc175bf6be4bee4
Jabah Gundy <gundy157@outlook.com>
2021-06-25 15:06:39 -0400

    Update

---
+++ june_18/Remove_Duplicates.js
@@ -0,0 +1,14 @@
+//2. remove duplicates
+const array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+const remover_D = function (array) {
+    if (array.length === 0) {
+        return 0;
+
+    }
+
+    let pointer1 = 1
+    for (let pointer2 = 1; pointer2 < array.length;
+        pointer2++) {
+        if ()
+    }
+}
